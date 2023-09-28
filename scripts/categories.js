05d9ccdc18fc89075fc6f5074a5d8a0e666f8989
kiran-naik1 <144588471+kiran-naik1@users.noreply.github.com>
2023-09-29 00:55:36 +0530

    Add files via upload

---
+++ scripts/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+    {
+        type : "Vegetarian" ,
+        icon : "assets/category-1.png",
+    },
+    {
+        type : "Breakfast" ,
+        icon : "assets/category-2.png",
+    },
+    {
+        type : "Beverages" ,
+        icon : "assets/category-3.png",
+    },
+    {
+        type : "Water" ,
+        icon : "assets/category-4.png",
+    },
+    {
+        type : "Non-Vegetarian" ,
+        icon : "assets/category-5.png",
+    }
+]
+
+export default categories;
---
+++ scripts/product.js
@@ -0,0 +1,33 @@
+const products = [
+    {
+        name : "Cheese Burger" ,
+        type: "1 UNIT",
+        tag : "trending_product",
+        price: 3.00,
+        img : 'assets/special-1.png'
+    },
+    {
+        name : "Fish Fry",
+        type: "1 unit",
+        tag : "trending_product",
+        price: 2.00,
+        img : 'assets/special-2.png'
+    },
+    {
+        name : "Broasted Chicken",
+        type: "1 plate",
+        tag : "trending_prodcut",
+        price: 5.00,
+        img : 'assets/special-3.png'
+    },
+    {
+        name : "Paneer Masala",
+        type: "1 plate",
+        tag : "trending_product",
+        price: 4.00,
+        img : 'assets/special-4.png'
+    },
+    
+]
+
+export default products;
